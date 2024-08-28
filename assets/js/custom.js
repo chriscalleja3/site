9905789291217ef38b67e62aecbf25e663c3879d
chriscalleja3 <chris.calleja.3@gov.mt>
2024-08-28 09:32:52 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,45 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all radio inputs that represent series options
-    var seriesInputs = document.querySelectorAll('input[name="series"]');
+        // Select the series input that is checked (selected by default or by the user)
+        var selectedSeriesInput = document.querySelector('input[name="series"]:checked');
 
-    if (seriesInputs.length > 0) {
-        seriesInputs.forEach(function(input) {
-            input.addEventListener('change', function() {
-                var selectedSeries = input.value;
-                var targetSeriesName = "Number of local breeds for which sufficient genetic resources are stored for reconstitution";
+        if (selectedSeriesInput) {
+            var selectedSeries = selectedSeriesInput.value;
+            var targetSeriesName = "Number of local breeds for which sufficient genetic resources are stored for reconstitution";
 
-                // If the selected series matches the one you want to hide tabs for
-                if (selectedSeries === targetSeriesName) {
-                    // Hide specific tabs
-                     var chartTab = document.querySelector('#tab-chartview');
-                     var chartPane = document.querySelector('#chartview');
-                     var mapTab = document.querySelector('#tab-mapview');
-                     var mapPane = document.querySelector('#mapview');
-                     
-                     if (chartTab) chartTab.style.display = 'none';
-                     if (chartPane) chartPane.style.display = 'none';
-                     if (mapTab) mapTab.style.display = 'none';
-                     if (mapPane) mapPane.style.display = 'none';
-                } else {
-                    // Ensure tabs are shown for other series
-                     var chartTab = document.querySelector('#tab-chartview');
-                     var chartPane = document.querySelector('#chartview');
+            // If the selected series matches the one you want to hide tabs and panes for
+            if (selectedSeries === targetSeriesName) {
+                // Hide specific tabs and their corresponding panes
+                var chartTab = document.querySelector('#tab-chartview');
+                var chartPane = document.querySelector('#chartview');
+                var mapTab = document.querySelector('#tab-mapview');
+                var mapPane = document.querySelector('#mapview');
 
-                     var mapTab = document.querySelector('#tab-mapview');
-                     var mapPane = document.querySelector('#mapview');
-
-                     if (chartTab) chartTab.style.display = 'block';
-                     if (chartPane) chartPane.style.display = 'block';
-
-                     if (mapTab) mapTab.style.display = 'block';
-                     if (mapPane) mapPane.style.display = 'block';
-                }
-            });
-
-            // Trigger change event on page load to ensure correct tabs are shown
-            if (input.checked) {
-                input.dispatchEvent(new Event('change'));
+                if (chartTab) chartTab.style.display = 'none';
+                if (chartPane) chartPane.style.display = 'none';
+                if (mapTab) mapTab.style.display = 'none';
+                if (mapPane) mapPane.style.display = 'none';
             }
-        });
-    }
+        }
 });
