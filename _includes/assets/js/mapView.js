ed8449a142bb1a326ae7e8d45830b926666ffcf8
chriscalleja3 <chris.calleja.3@gov.mt>
2024-09-13 11:17:07 +0200

    Create mapView.js

---
+++ _includes/assets/js/mapView.js
@@ -0,0 +1,26 @@
+var mapView = function () {
+
+  "use strict";
+
+  this.initialise = function(indicatorId, precision, precisionItems, decimalSeparator, dataSchema, viewHelpers, modelHelpers, chartTitles, startValues, proxy, proxySerieses, euSDG, euSDGSerieses, allObservationAttributes) {
+    $('.map').show();
+    $('#map').sdgMap({
+      indicatorId: indicatorId,
+      mapOptions: {{ site.map_options | jsonify }},
+      mapLayers: {{ site.map_layers | jsonify }},
+      precision: precision,
+      precisionItems: precisionItems,
+      decimalSeparator: decimalSeparator,
+      dataSchema: dataSchema,
+      viewHelpers: viewHelpers,
+      modelHelpers: modelHelpers,
+      chartTitles: chartTitles,
+      proxy: proxy,
+      proxySerieses: proxySerieses,
+      euSDG: euSDG, 
+      euSDGSerieses: euSDGSerieses,
+      startValues: startValues,
+      allObservationAttributes: allObservationAttributes,
+    });
+  };
+};
