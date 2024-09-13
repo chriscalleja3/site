610e5b69fc03ca866d80e3497c39b36297dc99e6
chriscalleja3 <chris.calleja.3@gov.mt>
2024-09-13 08:20:11 +0000

    Deploying to gh-pages from  @ ba3f20cd628d6e7723a28c8d0b5b0f95c56f6771 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4880,7 +4880,6 @@ function createIndicatorDownloadButtons(indicatorDownloads, indicatorId, el) {
     HIDE_SINGLE_SERIES: HIDE_SINGLE_SERIES,
     HIDE_SINGLE_UNIT: HIDE_SINGLE_UNIT,
     PROXY_PILL: PROXY_PILL,
-    EUSDG_PILL: EUSDG_PILL,
     initialiseFields: initialiseFields,
     initialiseUnits: initialiseUnits,
     initialiseSerieses: initialiseSerieses,
